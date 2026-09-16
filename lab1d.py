b8312a6ca7ea076b0d42d68ef640d692871a21d8
M4rcusLau <mlau56@myseneca.ca>
2026-09-16 20:09:45 +0000

    Completed lab1d

--- lab1d.py
+++ lab1d.py
@@ -9,18 +9,29 @@
 
 #TO-DO 1:
 #	Create a variable called "name" and assign it the value of your name.
+name="marcus"
 # Use the string method .upper() to convert the name to upper case.
+name=str.upper(name)
 # Create another variable called “age”, the value of “age” should be your age
+age=18
 # The script, when executed, should print out "How are you yourname? Happy xxth birthday!" To print this output use .format() method. 
-
+print("How are you",name,"? Happy",age,"th birthday!")
 #TO-DO 2:
 # Create a variable called "words".
 # The value of words should be "The quick brown fox jumps over the lazy dog".
 # Use indexing to return the first and 17th charecters of "words" to the user.
-
+words="The quick brown fox jumps over the lazy dog"
+print(words[0]) #First character
+print(words[16])
 #TO-DO 3:
 # Use negative indexing to return the words "jumps" and "quick" from "words" to the user.
-
+slice1=words[-23:-18]
+print(slice1)
+slice2=words[-39:-34]
+print(slice2)
 #TO-DO 4:
 # Use slicing to retun everything between index 2-15 to the user.
+slice3=words[2:15]
+print(slice3)
 # Print "uick brown foxs ju" from "words".
+print(words[5:22])
--- lab1e.py
+++ lab1e.py
@@ -14,4 +14,7 @@
 # The value of "stock" should also be a decimal number of your own choice.
 # Print the product of `quantity` and `stock` with 4 spaces before the answer using the module % formatting.
 # Then print the product of `quantity` and `stock` with 7 spaces before the answer and make sure the answer only goes to hundreadths (-.--) using the module % formatting.
-
+quantity=4.5
+stock=10.33333333
+product=quantity*stock
+print("The product of quantity and stock is : %4.0f" %product)
